b5229d7eb2847efdb305bdd61a9c6d372d63293a
leticialourenco <leticiagabriellourenco@gmail.com>
2016-04-20 17:51:24 -0700

    Writes Player class and methods

--- js/app.js
+++ js/app.js
@@ -59,7 +59,78 @@ Enemy.prototype.restart = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.col = 200;
+    this.row = 400;
+    this.points = 0;
+    this.sprite = 'images/char-cat-girl.png';
+};
 
+// Update the player's position
+Player.prototype.update = function(dt) {
+    // When it moves up after hitting the water block
+    // by going off-canvas
+    if (this.row <= -50) {
+        // Call to count point and refresh position
+        this.score();
+    }
+};
+
+// Draw the player and score number on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.col, this.row);  
+    // Set up the style for the score data to show and
+    // draw the score data on the canvas
+    ctx.fillStyle = "#FFF";
+    ctx.font = "bold 28px Impact";
+    ctx.strokeStyle = "#288E51";
+    ctx.lineWidth = 2;
+    ctx.fillText(this.points, 450, 631);
+    ctx.strokeText(this.points, 450, 631);
+};
+
+// Set up the movements of the player on the screen
+// when the keyes are hit on the keyboard 
+// movements happen when the player is NOT on the limits
+// of the canvas
+Player.prototype.handleInput = function(keyCode) {
+    if (keyCode == 'up') {
+        if (this.row >= -50) {
+            this.row -= 85;
+        }
+    } 
+    else if (keyCode == 'down') { 
+        if (this.row < 350) {
+            this.row += 85;
+        } 
+    }
+    else if (keyCode == 'right') { 
+        if (this.col <= 303) {
+            this.col += 101;
+        } 
+    }
+    else if (keyCode == 'left') { 
+        if (this.col >= 0) {
+            this.col -= 101;
+        } 
+    }
+};
+
+// Set the player to its initial positon and
+// add 1 point to the score
+Player.prototype.score = function() {
+    this.col = 200;
+    this.row = 400;
+    this.points += 1;
+};
+
+// Set the player to its initial positon and
+// set the score to 0
+Player.prototype.restart = function() {
+    this.col = 200;
+    this.row = 400;
+    this.points = 0;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
